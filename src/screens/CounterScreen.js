9df13bb0b7b4427483906d7fa5bf4c605e9646a2
gyoo <ernogarydaniel@gmail.com>
2024-03-15 17:47:04 +0800

    configured basic setup of redux. TODO: continue to build on nike app

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Button, } from 'react-native';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../features/counter/counterSlice';
 
 const CounterScreen = () => {
+
+  // initilize the selector
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>BIDET AKO</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      
+      {/* button style */}
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Button
+          title='Increment value'
+          onPress={() => dispatch(increment())}
+        />
+        <Text style={{ marginHorizontal: 10 }}>{count}</Text>
+        <Button 
+          title='Decrement value'
+          onPress={() => dispatch(decrement())}
+        />
+      </View>
     </View>
   )
 }
